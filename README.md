9f789e9e4035041cb9e0f27fd40b51b0bc8cafaf
Jared <jared.ekenstam@gmail.com>
2023-03-03 12:44:21 -0800

    first commit, v0.1

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # fedex-workaround
+Beta version of a tool to automatically format a fedex paper shipment document to fit a 4x6 label profile, and automatically print.
+
+To use:
+*Choose a directory to save fedex labels as pdf files to (This requires default settings to use a 4x6 pdf thermal label, and the default printer set to CutePDF Writer) 
+*Change the file paths in the code to match your fedex directory
+*Install this script and a third party command line tool PdftoPrinter to this same directory
+*Run this script.
---
+++ main.py
@@ -0,0 +1,61 @@
+from pypdf import PdfReader, PdfWriter
+import os
+from time import sleep
+
+def reset_eof_of_pdf_return_stream(pdf_stream_in:list):
+    # find the line position of the EOF
+    for i, x in enumerate(txt[::-1]):
+        if b'%%EOF' in x:
+            actual_line = len(pdf_stream_in)-i
+            print(f'EOF found at line position {-i} = actual {actual_line}, with value {x}')
+            break
+
+
+    # return the list up to that point
+    return pdf_stream_in[:actual_line]
+
+
+while True:
+    list = os.listdir('C:\\Users\\c.jared.eckenstam\\documents')
+    for item in list:
+        if ".pdf" in item:
+            # opens the file for reading
+            with open(item, 'rb') as p:
+                txt = (p.readlines())
+
+
+            # get the new list terminating correctly
+            txtx = reset_eof_of_pdf_return_stream(txt)
+
+            # write to new pdf
+            with open(item, 'wb') as f:
+                f.writelines(txtx)
+
+            #load new pdf
+            fixed_pdf = PdfReader(item)
+            pages = len(fixed_pdf.pages)
+            writer = PdfWriter()
+
+            #rotate 90 degrees
+            page = fixed_pdf.pages[0].rotate(90)
+
+            #crop to fit label, trim white space
+            page.mediabox.upper_right = (
+                page.mediabox.right * .91,
+                page.mediabox.top * .48,
+            )
+            page.trimbox.lower_left = (120, 90)
+            page.trimbox.upper_right = (556, 556)
+            page.cropbox.lower_left = (120, 90)
+            page.cropbox.upper_right = (556, 556)
+
+            #create output pdf from modified input pdf
+            writer.add_page(page)
+            with open("output.pdf","wb") as fp:
+                writer.write(fp)
+
+            #clean folder and print output
+            os.remove(item)
+            os.system('cmd /c "PdftoPrinter.exe output.pdf "ZDesigner ZT411-300dpi ZPL""')
+            os.remove('output.pdf')
+        sleep(3)
